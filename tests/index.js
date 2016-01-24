52f0e4eb4682c6f817372c3d5fdcce9a4975b33e
dbuarque <diogenes.buarque@gmail.com>
2016-01-24 03:30:53 -0200

    add some test functions for repack class

---
+++ tests/index.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+
+describe('Repack', function() {
+    it('#create() - Create a blank static webpack app');
+    it('#create() - Create a bootstrap webpack app');
+    it('#create() - Create a react webpack app');
+    it('#create() - Create a react / redux webpack app');
+    it('#create() - Create a react / tdd / bdd app');
+    it('#create() - Create a react / redux / tdd / bdd app');
+    it('#create() - Create a angularjs / redux webpack app');
+    it('#create() - Create a angularjs / tdd / bdd app');
+    it('#create() - Create a angularjs / tdd / bdd app');
+
+});
